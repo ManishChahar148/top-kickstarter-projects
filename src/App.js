fd647e74af52387cef0b16a02a2658613301c24d
Manish Chahar <manishchahar148@gmail.com>
2025-01-09 22:55:34 +0530

    kickstarter projects

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListProjects from './pages/ListProjects';
+
+function App() {
+  return (
+    <div className="App">
+     <ListProjects/>
+    </div>
+  );
+}
+
+export default App;
